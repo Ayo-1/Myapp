e182f654c879f0114143d217f16f6f34ab995c83
ayo-1 <ayomideoloyede95@gmail.com>
2019-11-15 17:27:59 +0600

    Updates

---
+++ precache-manifest.d4fa0ef7e271ce0c9002bbf4bb493a04.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed455913c444ee394245c3a74f42e420",
+    "url": "/Myapp/index.html"
+  },
+  {
+    "revision": "73869bdcbc20bf240513",
+    "url": "/Myapp/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "f532b0024e6d946d86cd",
+    "url": "/Myapp/static/css/main.4417e12e.chunk.css"
+  },
+  {
+    "revision": "73869bdcbc20bf240513",
+    "url": "/Myapp/static/js/2.e71766aa.chunk.js"
+  },
+  {
+    "revision": "f532b0024e6d946d86cd",
+    "url": "/Myapp/static/js/main.cbe9c936.chunk.js"
+  },
+  {
+    "revision": "40eb4eb4d0b4a47f377a",
+    "url": "/Myapp/static/js/runtime-main.833ae6a7.js"
+  },
+  {
+    "revision": "aa3666c542e403e4ab15d7d6205a8d50",
+    "url": "/Myapp/static/media/1215.aa3666c5.jpg"
+  },
+  {
+    "revision": "0a807b4337c8d9438b5479e50df7bbb4",
+    "url": "/Myapp/static/media/Ae.0a807b43.jpg"
+  },
+  {
+    "revision": "06e0d1e108b97e2a67342f171d3abeb4",
+    "url": "/Myapp/static/media/minionss.06e0d1e1.png"
+  },
+  {
+    "revision": "99b1f1a93a82ff6d15cc0f80be221ac1",
+    "url": "/Myapp/static/media/new1.99b1f1a9.jpg"
+  }
+]);
